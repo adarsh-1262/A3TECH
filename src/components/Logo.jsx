cc443515aed5d27a6f6022f10d978cc77b3c7c05
ADARSH SINGH <110599099+adarsh-1262@users.noreply.github.com>
2024-01-10 02:17:04 +0530

    logo updated.....

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '40%'}) {
   return (
     <div>
-       <b><img className='w-6 h-4' src="./src/assets/logo.jpeg" alt="" />A3 Blog...</b>
+       <b>A3 Blog...</b>
 
     </div>
   )
